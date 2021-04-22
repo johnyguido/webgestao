a5bf238ec374995beb7b034cd10133f0163a24b1
Johny Guido <johnyguido@hotmail.com.br>
2021-04-21 23:35:02 -0300

    Criação do projeto

---
+++ backend/src/main/java/com/nexti/webgestao/WebgestaoApplication.java
@@ -0,0 +1,13 @@
+package com.nexti.webgestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebgestaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgestaoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nexti/webgestao/WebgestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexti.webgestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebgestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
